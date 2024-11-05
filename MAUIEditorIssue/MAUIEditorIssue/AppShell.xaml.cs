491adbd234ce1a63418f138adbe7b6a84fc99554
Lakshminatarajan <50870908+Lakshminatarajan@users.noreply.github.com>
2024-11-05 13:46:14 +0530

    Add files via upload

---
+++ MAUIEditorIssue/MAUIEditorIssue/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUIEditorIssue
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MAUIEditorIssue/MAUIEditorIssue/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿namespace MAUIEditorIssue
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+    public class CustomStyleEditor : Editor
+
+    {
+
+#if WINDOWS
+ 
+        protected override void OnHandlerChanged()
+
+        {
+
+            // Hide editor border and underline.
+
+            var platformView = this.Handler?.PlatformView as Microsoft.UI.Xaml.Controls.TextBox;
+
+            if (platformView != null)
+
+            {
+
+                this.ApplyTextBoxStyle(platformView);
+
+            }
+ 
+ 
+            base.OnHandlerChanged();
+
+        }
+ 
+        private void ApplyTextBoxStyle(Microsoft.UI.Xaml.Controls.TextBox? textbox)
+
+        {
+
+            var textBoxStyle = new Microsoft.UI.Xaml.Style(typeof(Microsoft.UI.Xaml.Controls.TextBox));
+
+            textBoxStyle.Setters.Add(new Microsoft.UI.Xaml.Setter() { Property = Microsoft.UI.Xaml.Controls.Control.BorderBrushProperty, Value = new Microsoft.UI.Xaml.Media.SolidColorBrush(Windows.UI.Color.FromArgb(0, 0, 0, 0)) });
+
+            textBoxStyle.Setters.Add(new Microsoft.UI.Xaml.Setter() { Property = Microsoft.UI.Xaml.Controls.Control.BorderThicknessProperty, Value = new Thickness(0) });
+ 
+            textbox!.Resources.Add(typeof(Microsoft.UI.Xaml.Controls.TextBox), textBoxStyle);
+
+        }
+
+#endif
+
+    }
+
+
+}
